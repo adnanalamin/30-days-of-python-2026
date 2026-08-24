0aba7a1752d5860390cc661c4081ce47137b609d
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2026-08-24 16:51:10 +0600

    User Input type

--- Day-1/main.py
+++ Day-1/main.py
@@ -27,3 +27,29 @@ print(country)
 myname = "Al Amin"
 my_name = "Al Amin"
 myName = "Al Amin"
+
+#Data Type
+
+a = 20 #integer
+b = "Adnan" #String
+c = 20.20 #Float
+d = False #Boolean
+
+print(a, b, c, d)
+
+#Type
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+#User Input
+name = input("Inter Your Name :")
+print(name)
+
+
+#User Input type
+number = int(input("Inter A Number :"))
+print(number)
