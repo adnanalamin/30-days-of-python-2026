02b8584547ca5e92e8273b1d7ed537d1149268de
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2026-08-24 15:04:23 +0600

    Variable Naming Rules

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,3 +1,29 @@
 #Python Version Check
 import sys
 print(sys.version)
+
+#Print Function
+print("I love Bangladesh")
+
+
+# Single Quote and Double Quote
+
+print('my Country name is bangladesh')
+print("I love My Countey")
+
+
+#Variable
+
+name = "Al amin"
+age = 27
+country = 'Bangladesh'
+
+print(name)
+print(age)
+print(country)
+
+#Variable Naming Rules
+
+myname = "Al Amin"
+my_name = "Al Amin"
+myName = "Al Amin"
